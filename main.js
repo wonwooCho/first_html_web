ee689ee82a15a1b5e4856043c8dc6ee50e78935c
wonwooCho <jwwjjang@gmail.com>
2019-12-10 01:05:27 +0900

    change layout format

--- main.js
+++ main.js
@@ -3,47 +3,51 @@ var fs = require('fs');
 var url = require('url');
 
 var app = http.createServer(function (request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
+    var inURL = request.url;
+    var inData = url.parse(inURL, true);
+    var title = inData.query.id;
+    var pathName = inData.pathname;
 
-    console.log(`* URL: ${_url}`);
-    console.log(`* parsed query data: ${queryData.id}`);
+    console.log(inData);
 
-    if (_url == '/') {
-        title = 'Welcome';
-        queryData.id = 'index';
-    }
+    if (pathName == '/') {
 
-    if (_url == '/favicon.ico') {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
+        if (title == undefined) {
+            title = 'index';
+        }
 
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', (err, data) =>
-    {
-        if (err) throw err;
+        fs.readFile(`data/${title}`, 'utf8', (err, data) => {
 
-        var template = `
-        <!DOCTYPE html>
-    
-        <html>
-            <head>
-                <title>${title} - My Primitive Web</title>
-                <meta charset="utf-8">
-            </head>
+            var template = `
+            <!DOCTYPE html>
         
-            <body>
-                ${data}
-            </body>
-        </html>
-        `;
-
-        response.end(template);
-
-    });
+            <html>
+                <head>
+                    <title>${title} - My Primitive Web</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WELCOME TO MY WEB PAGE</a></h1>
+                <h2>
+                    <ol>
+                        <li><a href="/?id=about_me">About me</a></li>
+                        <li><a href="/?id=images">Images</a></li>
+                        <li><a href="/?id=paragraphs">Paragraphs</a></li>
+                    </ol>
+                </h2>
+                    <h2>${title}</h2>
+                    <p>${data}</p>
+                </body>
+            </html>
+            `;
+    
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Not Found');
+    }
 });
 
 app.listen(3000);
